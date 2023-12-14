79594359ed6617320b03a6d00bfe0875d2a29555
medamshiva20 <medamshiva20@gmail.com>
2023-12-14 20:46:04 +0530

    roboshop mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,48 +1,53 @@
-#!/bin/bash 
+#!/bin/bash
 
-USERID=$(id -u)
-LOGS_DIR=/tmp
+DATE=$(date +%F)
+LOGSDIR=/tmp
 # /home/centos/shellscript-logs/script-name-date.log
-DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
-LOG_FILE=$LOGS_DIR/$SCRIPT_NAME-$DATE.log
-
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ $USERID -ne 0 ]
- then
-    echo "$R ERROR:: Please run this script with root access $N"
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
- else
-    echo "INFO: You are root user"
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-       then 
-          echo -e "$2 ...$R FAILURE $N"
-          exit 1
-       else
-          echo -e "$2 ...$G SUCCESS $N"
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-cp mongo.repo /etc/yum.repo.d/mongo.repo &>>$LOG_FILE
 
-yum install mogodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installation of MongoDB" 
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
-systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Copied MongoDB repo into yum.repos.d"
+
+yum install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installation of MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
 VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongod &>>$LOG_FILE
+systemctl start mongod &>> $LOGFILE
+
 VALIDATE $? "Starting MongoDB"
 
-sed -i "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>>$LOG_FILE
-VALIDATE $? "Edited mongod conf"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
 
-systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Edited MongoDB conf"
+
+systemctl restart mongod &>> $LOGFILE
+
 VALIDATE $? "Restarting MonogoDB"

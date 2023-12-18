c285726b82de7b3efbbaf4212e48ff6667c66c07
medamshiva20 <medamshiva20@gmail.com>
2023-12-18 19:25:21 +0530

    roboshop redis

--- redis.sh
+++ redis.sh
@@ -1,47 +1,53 @@
-#!/bin/bash 
+#!/bin/bash
 
-USERID=$(id -u)
-LOG_DIR=/tmp
-DATE=$(date +%F:%H:%M:%S)
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOG_FILE=$LOG_DIR/$SCRIPT_NAME-$DATE.log
-
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ $USERID -ne 0 ]
- then
-    echo "ERROR:: Please run this script with root access"
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
- else
-    echo "INFO: You are root user"
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-     then 
+    if [ $1 -ne 0 ];
+    then
         echo -e "$2 ... $R FAILURE $N"
         exit 1
-     else
+    else
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG_FILE
 
-yum module enable redis:remi-6.2 -y &>>$LOG_FILE
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOGFILE
+
+VALIDATE $? "Installing Redis repo"
+
+yum module enable redis:remi-6.2 -y &>>$LOGFILE
+
 VALIDATE $? "Enabling Redis 6.2"
 
-yum install redis -y &>>$LOG_FILE
+yum install redis -y &>>$LOGFILE
+
 VALIDATE $? "Installing Redis 6.2"
 
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/redis.conf & /etc/redis/redis.conf &>>$LOG_FILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>$LOGFILE
+
 VALIDATE $? "Allowing Remote connections to redis"
 
 systemctl enable redis &>>$LOGFILE
+
 VALIDATE $? "Enabling Redis"
 
 systemctl start redis &>>$LOGFILE
+
 VALIDATE $? "Starting Redis"

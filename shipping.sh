3dfe3ee92e8ee6845615bfd9bb119bb2bc3c6856
medamshiva20 <medamshiva20@gmail.com>
2023-12-19 19:33:40 +0530

    roboshop cart and user

---
+++ shipping.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+username=roboshop
+directory=/app
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+yum install maven -y &>>$LOGFILE
+VALIDATE $? "Installing Maven"
+
+#once the user is created, if you run this script 2nd time
+# this command will defnitely fail
+# IMPROVEMENT: first check the user already exist or not, if not exist then create
+if id "$username" &> /dev/null
+  then 
+     echo "User $username already exist"
+  else
+     echo "User $username deos not exist.Creating user..."
+     useradd $username &>>$LOGFILE
+fi
+
+#write a condition to check directory already exist or not
+if [ -d "$directory"]
+ then 
+     echo "Directory $directory does not exist"
+ else
+     echo "Directory $directory already exist"
+     mkdir /app &>>$LOGFILE
+fi
+
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>>$LOGFILE
+
+VALIDATE $? "Downloading shipping artifact"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving to app directory"
+ 
+unzip -o /tmp/shipping.zip &>>$LOGFILE
+
+VALIDATE $? "Unzipping shipping"
+
+mvn clean package &>>$LOGFILE
+
+VALIDATE $? "packaging shipping app"
+
+mv target/shipping-1.0.jar shipping.jar &>>$LOGFILE
+
+VALIDATE $? "renaming shipping jar"
+
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
+
+VALIDATE $? "copying shipping service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon-reload"
+
+systemctl enable shipping  &>>$LOGFILE
+
+VALIDATE $? "Enabling shipping"
+
+systemctl start shipping &>>$LOGFILE
+
+VALIDATE $? "Starting shipping"
+
+
+yum install mysql -y  &>>$LOGFILE
+
+VALIDATE $? "Installing MySQL client"
+
+mysql -h 172.31.31.243 -uroot -pRoboShop@1 < /app/schema/shipping.sql  &>>$LOGFILE
+
+VALIDATE $? "Loaded countries and cities info"
+
+systemctl restart shipping &>>$LOGFILE
+
+VALIDATE $? "Restarting shipping"

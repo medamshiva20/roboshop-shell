2ce3aa0687126a37a431003a526d726063d45131
medamshiva20 <medamshiva20@gmail.com>
2023-12-23 21:02:53 +0530

    adding servers

--- create-ec2.sh
+++ create-ec2.sh
@@ -5,14 +5,15 @@ INSTANCE_TYPE=""
 IMAGE_ID="ami-081609eef2e3cc958"
 SECURITY_GROUP_ID="sg-001887f7841106c09"
 
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 for i in "${NAMES[@]}"
 do
-if [[ $i == "mongodb" || $i == "mysql"]]
- then
-    echo "$INSTANCE_TYPE="t3.medium"
- else
-    echo "$INSTANCE_TYPE="t2.medium"
-fi
+  if [[ $i == "mongodb" || $i == "mysql" ]];
+   then
+      INSTANCE_TYPE="t3.medium"
+   else
+      INSTANCE_TYPE="t2.medium"
+  fi
     echo "creating $i instance"
     aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type t2.micro --key-name kubernetes --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
